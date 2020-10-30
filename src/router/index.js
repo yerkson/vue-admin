943b8d1793d5441e4695961efbdb5ec27b3906a1
wangquan <328053660@qq.com>
2020-10-30 12:02:25 +0800

    '登录页面'

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../views/Login/index.vue';
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'login',
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
